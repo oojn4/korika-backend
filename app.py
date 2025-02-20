d206698b1a3f3f6b3d2c374611c4309df774fda4
Fauzan Faldy Anggita <fauzanfaldy@bps.go.id>
2025-02-20 22:09:43 +0700

    add parse password

--- app.py
+++ app.py
@@ -12,17 +12,20 @@ import requests
 from sqlalchemy.sql import text
 import pandas as pd
 import numpy as np
-
+import os
+import urllib.parse
 # Memuat variabel dari file .env
 load_dotenv()
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+
+
 # Konfigurasi database
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PASSWORD = urllib.parse.quote_plus(os.getenv('DB_PASSWORD'))
 DB_HOST = os.getenv('DB_HOST')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
